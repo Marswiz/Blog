c724a057b21a057144bd212123fa628501dde48b
Mars <marswiz@yeah.net>
2019-08-06 22:29:21 +0800

    尝试解决移动端多点触控问题

--- assets/js/navMobile.js
+++ assets/js/navMobile.js
@@ -1,30 +1,14 @@
 let touchstartPosX = 0;
 let touchendPosX = 0;
 
-function getStartPoint(e){
-		if (e.touches.length == 1){
-			touchstartPosX = e.touches[0].clientX;
-			console.log(e);
-			console.log('start point'+ touchstartPosX);
-		}
-}
-
-function getEndPoint(e){
-		if(e.changedTouches.length == 1){
-			touchendPosX = e.changedTouches[0].clientX;
-			console.log(e);
-			console.log('end point' + touchendPosX);
-		}
-}
-
 function navMoveFunc(e){	
-	if ((touchendPosX-touchstartPosX) >= 0.2*window.innerWidth && e.changedTouches.length == 1){
+	if ((touchendPosX-touchstartPosX) >= 0.2*window.innerWidth){
 		let a = document.querySelectorAll('#navMobile,#navMobile #navBlur,#navMobile #navTrans');
 		for (let i=0; i<a.length; i++){
 			a[i].style.animation = 'navMove 0.5s ease-in forwards';
 		}
 		console.log('Go'); 
-	} else if ((touchendPosX-touchstartPosX) <= -0.2*window.innerWidth && e.changedTouches.length == 1){
+	} else if ((touchendPosX-touchstartPosX) <= -0.2*window.innerWidth){
 		let a = document.querySelectorAll('#navMobile,#navMobile #navBlur,#navMobile #navTrans');
 		for (let i=0; i<a.length; i++){
 			a[i].style.animation = 'navMoveBack 0.5s ease-in forwards';
@@ -36,9 +20,33 @@ function navMoveFunc(e){
 	console.log('Reset to Null'); 
 }
 
+function getStartPoint(e){
+		if (e.touches.length == 1){
+			touchstartPosX = e.touches[0].clientX;
+			console.log(e);
+			console.log('start point'+ touchstartPosX);
+		}
+
+		//添加触摸结束事件监听器
+		document.body.addEventListener('touchend',navMoveFunc,false);
+}
+
+function gestureDetected(e){
+	return document.body.removeEventListener('touchend',navMoveFunc,false);
+	console.log(e);
+	console.log('手势检测到啦！');
+}
+
+function getEndPoint(e){
+		if(e.changedTouches.length == 1){
+			touchendPosX = e.changedTouches[0].clientX;
+			console.log(e);
+			console.log('end point' + touchendPosX);
+		}
+}
+
 if (window.innerWidth <= 768){
 	document.body.addEventListener('touchstart',getStartPoint,false);
-	// window.addEventListener('touchmove',navMobileFunc,false);
 	document.body.addEventListener('touchend',getEndPoint,false);
-	document.body.addEventListener('touchend',navMoveFunc,false);
+	document.body.addEventListener('gesturestart',gestureDetected,false);
 }
