0dc34dc6ce2ebe50b959f53b3868f377da47d645
Mars <marswiz@yeah.net>
2019-08-15 01:47:34 +0800

    ‘日夜切换按扭 git commit -m ‘日夜切换按扭

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,3 +1,6 @@
 window.addEventListener('load',navFunc,false);
 window.addEventListener('load',postsFunc,false);
 window.addEventListener('load',pagnaterFunc,false);
+if (window.innerWidth <= 768){
+	window.addEventListener('load',loadDayNight,false);
+}
--- assets/js/navMobile.js
+++ assets/js/navMobile.js
@@ -30,12 +30,12 @@ function getStartPoint(e){
 		}
 
 		//添加触摸结束事件监听器
-		document.body.addEventListener('touchend',navMoveFunc,false);
+		window.addEventListener('touchend',navMoveFunc,false);
 }
 
 function gestureDetected(e){	
 	console.log('手势检测到啦！');
-	return document.body.removeEventListener('touchend',navMoveFunc,false);
+	return window.removeEventListener('touchend',navMoveFunc,false);
 }
 
 function getEndPoint(e){
@@ -46,16 +46,109 @@ function getEndPoint(e){
 }
 
 if (window.innerWidth <= 768){
-	document.body.addEventListener('touchstart',getStartPoint,passiveSupported ? { passive: false } : false);
-	document.body.addEventListener('touchend',getEndPoint,passiveSupported ? { passive: false } : false);
-	document.body.addEventListener('gesturestart',gestureDetected,false);
+	window.addEventListener('touchstart',getStartPoint,passiveSupported ? { passive: false } : false);
+	window.addEventListener('touchend',getEndPoint,passiveSupported ? { passive: false } : false);
+	window.addEventListener('gesturestart',gestureDetected,false);
 }
 
+// 切换移动端日夜显示效果
+function changeImgColor(jqObject){
+	let a = jqObject.attr('src');
+	let b = a.replace(/Black/g,'White');
+	jqObject.attr('src',b);
+	return true;
+}
 
-// 调试
-// console.log('原始width:'+window.innerWidth);
-// function showVisual(){
-// 	console.log('scale is ' + visualViewport.scale);
-// 	console.log('width is ' + visualViewport.width);
-// }
-// document.body.addEventListener('touchend',showVisual,false);
+function changeImgColorBack(jqObject){
+	let a = jqObject.attr('src');
+	let b = a.replace(/White/g,'Black');
+	jqObject.attr('src',b);
+	return true;
+}
+
+function changeDayNight(){
+	if (window.innerWidth <= 768){
+	if (window.localStorage){
+		if( !window.localStorage.marswizBlogDayNight || window.localStorage.marswizBlogDayNight == 'Day'){
+			window.localStorage.setItem('marswizBlogDayNight','Night');	
+			console.log(window.localStorage.marswizBlogDayNight);
+		} else {
+			window.localStorage.setItem('marswizBlogDayNight','Day');
+			console.log(window.localStorage.marswizBlogDayNight);
+		}		
+	} else {
+		alert("Your Browser Doesn't Support Web Storage. The Function Cannot Be Used.");
+	}
+	}
+}
+
+function loadDayNight(){
+	if (window.innerWidth <= 768){
+		if(window.localStorage.marswizBlogDayNight == 'Day'){
+			$('body,.cloudTag,blockquote,#searchBox').css({
+				'backgroundColor':''
+			});
+			$('#blogBox #link,#blogs a,.blogYear,.tag,.cloudTag,#tagBlogs a,#aboutMeBox,#aboutMeTitle,#searchBox span').css({
+				'color':''
+			});
+			$('p#frontPostTagsMobile,.cloudTag').css({
+				'borderColor':''
+			});
+			$('#pageNumBox,#changeDayNight').css({
+				'color':''
+			});
+			$('div#socialBall').css({
+				'backgroundColor':''
+			});
+			$('div#navBlur').css({
+				'backgroundColor':'',
+				'boxShadow':''
+			});
+			let a = document.querySelectorAll('#pageNumBox,#changeDayNight,#postBox,.leancloud-visitors,.vat,.vcontent p,.vcontent a,.vwrap,input,textarea,.vinfo,.vinfo .vnum,.vlist,.vcount,div.vctrl,div.vctrl span');
+			for (let i=0; i<a.length; i++){
+				a[i].style.color = '';
+			}
+			$('#webInfo,.power a').css('color','');
+			$('path').attr('fill','');
+			$('#changeDayNight').attr('class','fa fa-lightbulb-o');
+			for (let i=0; i<$('img').length; i++){
+				changeImgColorBack($($('img')[i]));
+			}
+			$('pre').css('backgroundColor','');
+		} else {					
+			$('body,.cloudTag,blockquote,#searchBox').css({
+				'backgroundColor':'#506060'
+			});
+			$('#blogBox #link,#blogs a,.blogYear,.tag,.cloudTag,#tagBlogs a,#aboutMeBox,#aboutMeTitle,#searchBox span').css({
+				'color':'white'
+			});
+			$('p#frontPostTagsMobile,.cloudTag').css({
+				'borderColor':'white'
+			});
+
+			let a = document.querySelectorAll('#pageNumBox,#changeDayNight,#postBox,.leancloud-visitors,.vat,.vcontent p,.vcontent a,.vwrap,input,textarea,.vinfo,.vinfo .vnum,.vlist,.vcount,div.vctrl,div.vctrl span');
+			for (let i=0; i<a.length; i++){
+				a[i].style.color = 'white';
+			}
+
+			$('#webInfo,.power a').css('color','#999');
+			$('path').attr('fill','white');
+			$('div#socialBall').css({
+				'backgroundColor':'#bdb7b1'
+			});
+			$('div#navBlur').css({
+				'backgroundColor':'#506060',
+				'boxShadow':'0 0 3px #6cd1ef'
+			});
+			$('#changeDayNight').attr('class','fa fa-moon-o');
+			for (let i=0; i<$('img').length; i++){
+				changeImgColor($($('img')[i]));
+			}
+			$('pre').css('backgroundColor','#999');
+		
+		}
+	}
+}
+
+$('#changeDayNight').click(changeDayNight);
+$('#changeDayNight').click(loadDayNight);
