da192c87993b8e53a4d66f59de21cb3ee1af940c
Marswiz <marswiz@yeah.net>
2021-03-10 14:26:08 +0800

    '20210310_Update:Js_optimized.'

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,6 +1,6 @@
-window.addEventListener('load',navFunc,false);
-window.addEventListener('load',postsFunc,false);
-window.addEventListener('load',pagnaterFunc,false);
 if (window.innerWidth <= 768){
 	window.addEventListener('load',loadDayNight,false);
 }
+window.addEventListener('load',navFunc,false);
+window.addEventListener('load',postsFunc,false);
+window.addEventListener('load',pagnaterFunc,false);
